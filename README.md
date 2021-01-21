c96b1f4576fe5ecadf551b2aa12cc28d93b4bc8f
noyobo <noyobo@gmail.com>
2021-01-21 20:10:55 +0800

    test: upgrade

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# esm2cjs
+# esm-cjs
+
 Transform ESM to Common JS for present NodeJS, without any junk wrappers or useless renaming.
+
+## TODO
+
+- [ ] Aggregating modules
--- test/export.test.js
+++ test/export.test.js
@@ -1,8 +1,8 @@
 const transform = require('../lib/transform');
 
 describe('transform export', () => {
+  let code;
   it('should exporting individual features', () => {
-    let code;
     code = transform(`export let a, b, c;`);
     expect(code).toMatchSnapshot();
     code = transform(`export let a = 1, b = 2, c = 3;`);
@@ -19,15 +19,11 @@ describe('transform export', () => {
   });
 
   it('should renaming exports', () => {
-    const code = transform(`var variable1 = 1;
-var variable2 = 2;
-
-export { variable1 as name1, variable2 as name2 };`);
+    const code = transform(`var variable1 = 1; var variable2 = 2; export { variable1 as name1, variable2 as name2 };`);
     expect(code).toMatchSnapshot();
   });
 
   it('should default exports', () => {
-    let code;
     code = transform(`export default true`);
     expect(code).toMatchSnapshot();
     code = transform(`export default function () {  }`);
@@ -36,5 +32,12 @@ export { variable1 as name1, variable2 as name2 };`);
     expect(code).toMatchSnapshot();
     code = transform(`export default class ClassName {}`);
     expect(code).toMatchSnapshot();
+  });
+
+  it('should aggregating modules', () => {
+    const fn = jest.fn(() => {
+      transform(`export { name1, name2 } from 'foo'`);
+    });
+    expect(fn).toThrowErrorMatchingSnapshot();
   });
 });
